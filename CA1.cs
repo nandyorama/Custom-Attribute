15d4b28b681ba6e5bf58741acfa877a76443893b
pramod <pramod.nandy06@gmail.com>
2017-04-17 00:31:09 +0530

    Update CA1.cs
    Used Reflection to get Properties and then loop through all attributes.

--- CA1.cs
+++ CA1.cs
@@ -1,54 +1,54 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace CustomAttribute1
-//{
-//    [AttributeUsage(AttributeTargets.Property)]
-//    class HelpAttribute : Attribute
-//    {
-//        public int MaxLength { get; set; }
-//    }
+namespace CustomAttribute1
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class HelpAttribute : Attribute
+    {
+        public int MaxLength { get; set; }
+    }
 
-//    class Customer
-//    {
-//        private string _CustomerId;
+    class Customer
+    {
+        private string _CustomerId;
 
-//        [HelpAttribute(MaxLength = 10)]
-//        public string CustomerId
-//        {
-//            get { return _CustomerId; }
-//            set { _CustomerId = value; }
-//        }
-//    }
+        [HelpAttribute(MaxLength = 10)]
+        public string CustomerId
+        {
+            get { return _CustomerId; }
+            set { _CustomerId = value; }
+        }
+    }
 
-//    class QueryApp
-//    {
-//        public static void Main()
-//        {
-//            Customer obj = new Customer();
-//            obj.CustomerId = "12345678901";
+    class Sample
+    {
+        public static void Main()
+        {
+            Customer obj = new Customer();
+            obj.CustomerId = "12345678901";
 
-//            Type type = typeof(Customer);
-//            HelpAttribute HelpAttr;
+            Type type = typeof(Customer);
+            HelpAttribute HelpAttr;
 
-//            foreach (System.Reflection.PropertyInfo p in type.GetProperties())
-//            {
-//                // for every property loop through all attributes
-//                foreach (Attribute attr in p.GetCustomAttributes(false))
-//                {
-//                    HelpAttr = attr as HelpAttribute;
-//                    if (p.Name == "CustomerId")
-//                    {
-//                        // Do the length check and and raise exception accordingly
-//                        if (obj.CustomerId.Length > HelpAttr.MaxLength)
-//                        {
-//                            throw new Exception(" Max length issues ");
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+            foreach (System.Reflection.PropertyInfo p in type.GetProperties())
+            {
+                // for every property loop through all attributes
+                foreach (Attribute attr in p.GetCustomAttributes(false))
+                {
+                    HelpAttr = attr as HelpAttribute;
+                    if (p.Name == "CustomerId")
+                    {
+                        // Do the length check and and raise exception accordingly
+                        if (obj.CustomerId.Length > HelpAttr.MaxLength)
+                        {
+                            throw new Exception(" Max length issues ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
