cbe1a8eb924e6c8153c5f15b6b70f1b1761c93fa
pramod <pramod.nandy06@gmail.com>
2017-04-17 00:21:00 +0530

    Sample C# Program for Use of Attribute.

---
+++ Sample.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace CustomAttribute1
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            // Warning: 'Program.Test()' is obsolete
+//            Test();
+//            Test1();
+//            //Test2();//It will be error if not commented
+//        }
+
+//        [Obsolete]
+//        static void Test()
+//        {
+//        }
+//        [Obsolete("This method is no longer Exits")]
+//        static void Test1()
+//        {
+//        }
+//        [Obsolete("This method is no longer Exits",true)]
+//        static void Test2()
+//        {
+//        }
+//    }
+//}
